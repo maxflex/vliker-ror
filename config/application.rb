41963b67e84c5f833e608742c47b653ccbc568c5
Max Kolyadin <makcyxa-k@yandex.ru>
2015-10-18 23:15:39 +0300

    uglifier fix2

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Vliker
     config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.paths << Rails.root.join('app', 'assets', 'templates')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end

419d70310c2b5f322940b25f1ef1e1bac8435ffc
Max Kolyadin <makcyxa-k@yandex.ru>
2016-01-01 14:42:08 +0300

    fix

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@ class Notification
 
   # count notifications
   def self.count(user_id)
-    if user_id > 0
+    if !user_id.nil?
       Task.where(user_id: user_id, active: true).count + Order.where(user_id: user_id, done: false).count
     end
   end
